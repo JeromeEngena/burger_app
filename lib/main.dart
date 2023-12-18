810816fc6f62962483cfca1ab0067c57247729c6
Jerome <jembrian7@gmail.com>
2023-12-19 00:03:49 +0300

    built header

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'header.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Hamburger(),
+      home: const Hamburger(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: const AppBarTheme(color: Colors.teal, centerTitle: true),
@@ -30,14 +32,20 @@ class _HamburgerState extends State<Hamburger> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(slivers: [
-      SliverAppBar(
-        title: const Text('Deliver Me'),
-        leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
-        actions: [
-          IconButton(icon: const Icon(Icons.shopping_cart), onPressed: () {})
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            title: const Text('Deliver Me'),
+            leading: IconButton(icon: const Icon(Icons.menu), onPressed: () {}),
+            actions: [
+              IconButton(
+                  icon: const Icon(Icons.shopping_cart), onPressed: () {}),
+            ],
+          ),
+          const Header(),
         ],
       ),
-    ]));
+    );
   }
 }
